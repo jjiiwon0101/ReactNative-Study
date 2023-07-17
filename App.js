16b270b58a87cd31a2700a08d6c87da488018ca7
jiwon <kjwn0241@naver.com>
2023-07-17 16:16:22 +0900

    230717 삭제 구현 완료. 입력 양식을 모달로 구현 중

--- App.js
+++ App.js
@@ -1,14 +1,66 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import { Button, FlatList, StyleSheet, TextInput, View } from "react-native";
+
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
+  const [ModalIsVisible, setModalIsVisible] = useState(false);
+  const [todoGoals, setTodoGoals] = useState([]);
+
+  //할 일 추가 모달을 띄워주는 함수
+  const startAddGoalHandler = () => {
+    setModalIsVisible(true);
+  };
+
+  //버튼을 누르면 할 일 목록을 추가하는 함수
+  const addGoalHandler = (enteredGoalText) => {
+    //useState로 관리하는 상태 변수의 setter 안에 콜백 함수를 작성하면,
+    //그 콜백 함수의 매개값은 항상 해당 상태 변수의 최신 값이 전달됩니다.
+    setTodoGoals((currentTodoGoals) => [
+      ...currentTodoGoals,
+      { text: enteredGoalText, id: Math.random().toString() },
+    ]);
+  };
+
+  const deleteGoalHandler = (id) => {
+    setTodoGoals((currentTodoGoals) => {
+      return currentTodoGoals.filter((goal) => goal.id !== id);
+    });
+  };
+
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="할 일을 입력하세요" />
-        <Button title="할 일 추가하기(✿◕‿◕✿)" />
-      </View>
-      <View>
-        <Text>할 일 목록들...</Text>
+      <Button
+        title='할 일 추가하기!'
+        color='#4FC3F7'
+        onPress={startAddGoalHandler}
+      />
+      {ModalIsVisible && <GoalInput onAddGoal={addGoalHandler} />}
+      <View style={styles.goalsContainer}>
+        {/* ScrollView는 전체 화면이 렌더링 될 때 안의 항목들을 전부 렌더링합니다.
+            이로 인해, 성능의 저하가 발생할 수 있습니다.
+            (보이지 않는 영역까지 렌더링을 진행하기 때문에 목록이 많다면 로딩이 길어짐.) 
+            FlatList는 보이는 영역만 일단 렌더링을 진행하고, 나머지 항목들은
+            스크롤 움직임이 발생하면 렌더링을 진행합니다. */}
+        <FlatList
+          data={todoGoals}
+          renderItem={(itemData) => {
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                id={itemData.item.id}
+                onDeleteItem={deleteGoalHandler}
+              />
+            );
+          }}
+          keyExtractor={(item, index) => {
+            // console.log("item: ", item);
+            // console.log("index: ", index);
+            return item.id;
+          }}
+          alwaysBounceVertical={false}
+        ></FlatList>
       </View>
     </View>
   );
@@ -16,6 +68,11 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  goalsContainer: {
+    flex: 4,
   },
 });
